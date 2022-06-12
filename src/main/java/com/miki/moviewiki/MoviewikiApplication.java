98a8eaeafb647fcce285803d1738c3c3b922a8c4
mhlee <mihyun0921@gmail.com>
2022-06-13 00:01:01 +0900

    first commit

---
+++ src/main/java/com/miki/moviewiki/MoviewikiApplication.java
@@ -0,0 +1,13 @@
+package com.miki.moviewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviewikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviewikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miki/moviewiki/MoviewikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miki.moviewiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
